cacefd3325bd1a56fa73aa69dee42da0c0e16eb2
maxcravo <maxcravo25@gmail.com>
2024-05-16 12:03:46 -0300

    commitando arquivos relacionados a conteirização do servidor kafka e do projeto node

--- api/index.js
+++ api/index.js
@@ -1,63 +1,115 @@
-require('dotenv/config')
+require('dotenv/config');
 
-/* scholar api */
-const scholarFetcher = require('./scholarFetcher');
+const { newKafkaConsumer, newKafkaProducer, admin } = require('./kafkajsConfig');
 
-async function test() {
-	const results = await scholarFetcher.fetchScholarAPI("deep learning");
-	// enviar pro producer etc
-	console.log(`results = ${JSON.stringify(results)}`);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.write("Heloo Kafka");
+	res.end();
+});
+
+async function adminInit() {
+	admin.connect();
+	await admin.createTopics({
+		topics: [
+			{
+				topic: 'test.topic2',
+				numPartitions: 2,
+				// replicationFactor: 1
+			},
+		],
+	});
+	await admin.disconnect();
+}
+
+async function producerInit() {
+  const producer = newKafkaProducer();
+  await producer.connect();
+ 
+  await producer.send({
+    topic: 'test.topic2',
+    messages: [
+      {
+        partition: 0,
+        key: 'just-a-key-3',
+        value: 'Hello Kafka 3!', 
+      },
+			{
+        partition: 0,
+        key: 'just-a-key-4',
+        value: 'Hello Kafka 4!',
+      },
+    ],
+  });
+  await producer.disconnect();
+}
+
+async function consumerInit() {
+	const consumer = newKafkaConsumer({ groupId: 'test-group' });
+	await consumer.connect();
+
+	await consumer.subscribe({ topics: ['test.topic'], fromBeginning: true });
+
+	await consumer.run({
+		eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
+			console.log({
+				topic,
+				partition,
+				key: message.key.toString(),
+				value: message.value.toString(),
+			});
+		},
+	});
+
+
+	// console.log(`describeGroup = ${JSON.stringify(await consumer.describeGroup())}`);
+
+	// const topic = 'topic.test'
+	// const consumer = newKafkaConsumer({ groupId: 'test-group' })
+	
+	// const run = async () => {
+	// 	await consumer.connect()
+	// 	await consumer.subscribe({ topic, fromBeginning: true })
+	// 	await consumer.run({
+	// 		eachMessage: async ({ topic, partition, message }) => {
+	// 			const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+	// 			console.log(`- ${prefix} ${message.key}#${message.value}`)
+	// 		},
+	// 	})
+	// }
+	
+	// run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+	
+}
+
+async function test(){
+	// await adminInit();
+	await producerInit();
+	await consumerInit();
+
+	await admin.connect();
+	console.log(`topicMeta = ${JSON.stringify(await admin.fetchTopicMetadata({ topics: ['test.topic2']}))}`);
+	await admin.disconnect();
 }
 
 test();
 
-/* KAFKA */
-// const { newKafkaProducer, newKafkaConsumer, admin } = require('./kafkajsConfig.js');
+/* scholar api */
+// const scholarFetcher = require('./scholarFetcher');
 
-// const myProducer = newKafkaProducer();
-
-// const myConsumer = newKafkaConsumer('test-group');
-// const myadmin = admin();
-
-// async function createArticleTopic() {
-//   await myProducer.connect();
-//   await myProducer.send({
-//     topic: 'test-article',
-//     messages: [
-//       {value: 'test2'}
-//     ]
-//   })
-
+// async function test() {
+// 	const results = await scholarFetcher.fetchScholarAPI("deep learning");
+// 	// enviar pro producer etc
+// 	console.log(`results = ${JSON.stringify(results)}`);
 // }
 
-// createArticleTopic();
+// test();
 
-// async function readMessages() {
-//   myConsumer.connect();
-//   await myConsumer.subscribe({ topic: 'test-article', fromBeginning: true })
-
-//   await myConsumer.run({
-//     eachMessage: async ({ topic, partition, message }) => {
-//       console.log({
-//         value: message.value.toString(),
-//       })
-//     },
-//   })
-// }
-
-// readMessages()
-
-// async function deleteTopics(topics) {
-//   console.log("delete topics");
-//   myadmin.connect()
-// 	await myadmin.listTopics()
-
-// 	await myadmin.deleteTopics({
-// 		topics
-// 	})
-// 	await myadmin.listTopics()
-
-// 	myadmin.disconnect()
-// }
-
-// deleteTopics(["test-article"])
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
--- api/kafkajsConfig.js
+++ api/kafkajsConfig.js
@@ -1,51 +1,21 @@
-const { Kafka, Partitioners } = require('kafkajs')
+const { Kafka, Partitioners } = require('kafkajs');
 
 const kafka = new Kafka({
 	clientId: 'my-app',
 	brokers: ['localhost:9092'],
+	retry: {
+    initialRetryTime: 100,
+    retries: 5
+  }
 })
 
-const newKafkaProducer = () => kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner })
+const newKafkaProducer = () => kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner, allowAutoTopicCreation: false });
+const newKafkaConsumer = (groupId) => kafka.consumer({ groupId, allowAutoTopicCreation: false });
 
-const newKafkaConsumer = (groupId) => kafka.consumer({groupId});
-
-const admin = () => kafka.admin() 
+const admin = kafka.admin();
 
 module.exports = {
 	admin,
 	newKafkaProducer,
 	newKafkaConsumer
 }
-
-// async function connectProducer() {
-// 	await producer.connect()
-// 	await producer.send({
-// 		topic: 'article',
-// 		messages: [
-// 			{ value: 'Hello KafkaJS user!' },
-// 			{ value: 'Here\'s an article https://ieeexplore.ieee.org/abstract/document/10213406!' },
-// 		],
-// 	})
-
-// 	await producer.disconnect()
-// }
-
-// connectProducer()
-
-
-// const consumer = kafka.consumer({ groupId: 'test-group' })
-
-// async function connectConsumer() {
-//   await consumer.connect()
-//   await consumer.subscribe({ topic: 'article', fromBeginning: true })
-
-// 	await consumer.run({
-// 		eachMessage: async ({ topic, partition, message }) => {
-// 			console.log({
-// 				value: message.value.toString(),
-// 			})
-// 		},
-// 	})
-// }
-
-// connectConsumer();
