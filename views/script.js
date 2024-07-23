380c7da672d77ba84f5c9476c94b3a0744ef7f89
maxcravo <maxcravo25@gmail.com>
2024-07-23 11:45:35 -0300

    ajustando

--- views/script.js
+++ views/script.js
@@ -1,12 +1,19 @@
 //! Importar o handleSearch aqui 
+import handleSearch from "../handleSearch"
 
 export default function sendData() {
   let number_results = document.getElementById("query_quantity").value
   let query = document.getElementById("query_text").value
-  //TODO colocar condição onclick do button de submit
+  let submit = document.getElementById("submit_button").onclick
   const results = {
     query: query,
     numberOfResults: number_results
   }
+  //TODO colocar condição onclick do button de submit
+  if (submit) {
+    handleSearch(results)
+  }
+
+  
 return results
 }
