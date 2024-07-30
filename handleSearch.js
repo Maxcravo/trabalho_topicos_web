ddc4405c21a73b74da4d41ef904a8fd0e22569cd
maxcravo <maxcravo25@gmail.com>
2024-07-30 09:20:23 -0300

    front-end

--- handleSearch.js
+++ handleSearch.js
@@ -1,12 +1,18 @@
-import results from "./views/script";
-
-console.log(results);
+const query_text = document.getElementById("query_text");
+const submit = document.querySelector("#submit_button");
 
 async function handleSearch({ query, numberOfResults = 15 }) {
   try {
-
-    // ver se tem a query 
-    // chama o scholar fetcher (const queryResults = await fetch scholar)
+    //! ver se tem a query 
+    if (!query) {
+      throw new Error("não foi enviada query")
+    }
+    //! chama o scholar fetcher (const queryResults = await fetch scholar)
+    // Passar na requisição o query
+    const queryResults = await fetch("http://localhost:3001/")
+    
+    queryResults 
+    
     // pegar os resultados do scholar fetcher e entrega para o producer colocar na fila do kafka
     // chamr o consumer para fazer o tratamento dos dados
     
@@ -15,4 +21,8 @@ async function handleSearch({ query, numberOfResults = 15 }) {
   }
 }
 
-export default handleSearch
+submit.addEventListener("click", ()=> {
+  handleSearch({query:query_text.value});
+  console.log(query_text.value);
+})
+
